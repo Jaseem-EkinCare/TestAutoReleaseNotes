5f1cc183d64a2f87c7097b3394a2b27e59536b90
Jaseemakhtar <jaseemakhtar@ekincare.com>
2022-04-13 16:47:17 +0530

    * Adds another text view

--- app/src/main/java/com/example/composesamples/MainActivity.kt
+++ app/src/main/java/com/example/composesamples/MainActivity.kt
@@ -3,9 +3,7 @@ package com.example.composesamples
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -13,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composesamples.ui.theme.ComposeSamplesTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,6 +37,8 @@ fun Greeting(name: String) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(text = name)
+        Spacer(modifier = Modifier.height(16.dp))
         Text(text = name)
     }
 }
